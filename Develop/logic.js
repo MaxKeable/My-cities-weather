20486f4f0a208726ef9f80c0908904e20dd8300d
MaxKeable <max.keable488@gmail.com>
2023-03-18 16:08:05 +1000

    adding html elemets and began styleing

---
+++ Develop/logic.js
@@ -0,0 +1,6 @@
+var current = document.getElementById("current-day");
+
+var currentdaydisplay = dayjs().format("dddd, MMMM D, YYYY");
+console.log(currentdaydisplay);
+
+current.textContent = currentdaydisplay;
