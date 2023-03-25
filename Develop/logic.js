c44a34a69ad70c0f854b898ba3d1db21a2a5bd04
MaxKeable <max.keable488@gmail.com>
2023-03-25 13:22:32 +1000

    finalizing html styling and begin returing data from api into html

--- Develop/logic.js
+++ Develop/logic.js
@@ -2,28 +2,10 @@
 
 var current = $("#current-day");
 var currentdaydisplay = dayjs().format("dddd, MMMM D, YYYY");
-var weather = [
-    {
-        city: 'Brisbane',
-        temp: 20, 
-        wind: 10, 
-        humidity: 50, 
-        uv: 6, 
-    },
-    {
-        city: 'Sydney',
-        temp: 15,
-        wind: 5,
-        humidity: 40,
-        uv: 5,
-    }
-];
-var temp = $('#temp');
-var wind = $('#wind');
-var humidity = $('#humidity');
-var uv = $('#uv');
-var card = $('#card');
-var cityTitle = $('.card-title');
+var cityTitle1 = $("#city-title1");
+var date1 = $("#date1");
+var temperature1 = $("#temperature1");
+var windSpeed1 = $("#wind-speed1");
 
 
 current.text(currentdaydisplay);
@@ -36,25 +18,15 @@ var APIKey = '25d9d63d4bd3a2a927968ea3df474bf8';
 
 button.on("click", function() {
     var city = $("#search-input").val();
-    if (city === 'Location') {
+    if (city === 'Enter Your Location') {
         return
-    } else if (city !== '') {
-    $('#search-content').removeClass('search-content');
-    $('#card-div').removeClass('hide');
-    $('#card-div').addClass('card-display'); 
-    localStorage.setItem('city', city);
-    cityTitle.text(localStorage.getItem('city'));
-    temp.text(weather[0].temp);
+    } else if (city !== 'Enter Your Location') {
+    localStorage.setItem('city1', city);
+    cityTitle1.text(localStorage.getItem('city'));
     }
 
-    fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' +city+'&limit=5&appid='+APIKey).then (Response => Response.json()).then (data => {
+    fetch ('http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+APIKey).then (Response => Response.json()).then (data => {
     console.log(data);
 })
   });
-
-  
-
-// fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' +city+'&limit=5&appid='+APIKey).then (Response => Response.json()).then (data => {
-//     console.log(data);
-// })
 
